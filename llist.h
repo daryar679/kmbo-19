368a71887f4effc5140793d8f444cca4ed19c896
daryar679 <58885122+daryar679@users.noreply.github.com>
2020-04-16 14:05:12 +0300

    Update llist.h

--- llist.h
+++ llist.h
@@ -8,21 +8,21 @@
 class LList
 {
 	struct Node {
-		Node(const double& value, Node* next = nullptr);
+		Node(const int& value, Node* next = nullptr);
 		~Node();
 
-		void insertNext(const double& value);
-		void removeNext();
+		void insertNext(const int& value);
 
-		ValueType value;
+		int value;
 		Node* next;
 	};
-	
-	public:
+
+public:
 
 	LList();                         // construct new collection
 	~LList();                        // free resources
 
+	LList::Node* getNode(const size_t idx) const;
 	void push_back(int val);         // add new value at the end:  [1 2 3 4] -> [1 2 3 4 5]
 	void push_front(int val);        // add new value at the begin [1 2 3 4] -> [5 1 2 3 4]
 	void pop_back(int val);          // remove at the end          [1 2 3 4] -> [1 2 3]
@@ -33,11 +33,11 @@ class LList
 	void erase_at(size_t idx);       // remove item at specific position: [1 2 3 4], 2 -> [1 2 4]
 	void insert_at(size_t idx, int val); // insert item at specific position: [1 2 3 4], 1, 5 -> [1 5 2 3 4]
 	void reverse();                  // reverse item sequense: [1 2 3 4] -> [4 3 2 1]
-};
+
 private:
 	Node* _head;
 	size_t	_size;
 
 	void forceNodeDelete(Node* node);
-
+};
 #endif //LLIST_H
